1d2a8cbd5d7e486ca5ef27fc9a0d6ff98e6fe8c1
Jacob Bare <jacob@parameter1.com>
2021-10-22 12:03:02 -0500

    Fix docblock grammar

--- packages/object-path/src/get-as-array.js
+++ packages/object-path/src/get-as-array.js
@@ -2,7 +2,7 @@ import { asArray } from '@parameter1/marko-base-cms-utils';
 import get from './get.js';
 
 /**
- * Gets a object path value (via dot-notation) as an array.
+ * Gets an object path value (via dot-notation) as an array.
  *
  * @param {object} obj
  * @param {string} path
