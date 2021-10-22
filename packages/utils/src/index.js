05a1fe53b635e6b45dc68d3049c6e4927030ac62
Jacob Bare <jacob@parameter1.com>
2021-10-22 11:00:23 -0500

    Create generic utils package

---
+++ packages/utils/src/index.js
@@ -0,0 +1,4 @@
+export { default as asArray } from './as-array.js';
+export { default as asObject } from './as-object.js';
+export { default as isFunction } from './is-function.js';
+export { default as isObject } from './is-object.js';
