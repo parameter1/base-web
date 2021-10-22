d754c776a2b2e779e6540c8181d7bca9ce0caf6d
Jacob Bare <jacob@parameter1.com>
2021-10-22 12:28:00 -0500

    Create immediately throw util

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,5 +1,6 @@
 export { default as asArray } from './as-array.js';
 export { default as asObject } from './as-object.js';
+export { default as immediatelyThrow } from './immediately-throw.js';
 export { default as isFunction } from './is-function.js';
 export { default as isObject } from './is-object.js';
 export { default as wait } from './wait.js';
