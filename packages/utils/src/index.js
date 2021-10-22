3a194f546aa651b0aaa945b02d112a982f2e1f50
Jacob Bare <jacob@parameter1.com>
2021-10-22 11:22:35 -0500

    Create wait/sleep util

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -2,3 +2,4 @@ export { default as asArray } from './as-array.js';
 export { default as asObject } from './as-object.js';
 export { default as isFunction } from './is-function.js';
 export { default as isObject } from './is-object.js';
+export { default as wait } from './wait.js';
