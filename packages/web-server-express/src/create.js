d4e5e78dd05337f2ec2c1ea23c631b5b6d440023
Jacob Bare <jacob@parameter1.com>
2021-10-23 10:31:36 -0500

    Add siteId and tenantKey params; pass to graphql client

--- packages/web-server-express/src/create.js
+++ packages/web-server-express/src/create.js
@@ -13,21 +13,35 @@ const { env } = process;
  *
  * @param {object} params
  * @param {string} params.baseCMSGraphQLURL
+ * @param {string} params.siteId
+ * @param {string} params.tenantKey
  */
 export default async (params = {}) => {
   const {
     baseCMSGraphQLURL,
+    siteId,
+    tenantKey,
   } = await validateAsync(Joi.object({
-    baseCMSGraphQLURL: Joi.string().trim().uri().default(env.BASE_CMS_GRAPHQL_URL || '')
-      .required(),
-  }).required(), params);
+    baseCMSGraphQLURL: Joi.string().trim().uri().required(),
+    siteId: Joi.string().trim().pattern(/^[a-f0-9]{24}$/).required(),
+    tenantKey: Joi.string().trim().pattern(/^[a-z0-9]+_[a-z0-9]+$/).required(),
+  }).required(), {
+    ...params,
+    baseCMSGraphQLURL: params.baseCMSGraphQLURL || env.BASE_CMS_GRAPHQL_URL,
+    siteId: params.siteId || env.SITE_ID,
+    tenantKey: params.tenantKey || env.TENANT_KEY,
+  });
 
   const server = express();
   // Add cookie parsing.
   server.use(cookieParser());
 
   // Set BaseCMS Apollo client.
-  server.use(apollo({ prop: '$apolloBaseCMS', uri: baseCMSGraphQLURL }));
+  server.use(apollo({
+    prop: '$apolloBaseCMS',
+    uri: baseCMSGraphQLURL,
+    link: { headers: { 'x-tenant-key': tenantKey, 'x-site-id': siteId } },
+  }));
 
   server.get('/', (_, res) => res.json({ hello: 'world' }));
   return server;
--- packages/web-server-fastify/test/index.js
+++ packages/web-server-fastify/test/index.js
@@ -2,4 +2,6 @@ import run from '../src/index.js';
 
 run({
   baseCMSGraphQLURL: 'https://graphql.virgon.base.parameter1.com',
+  siteId: '5fce561dd28860bc33b823ce',
+  tenantKey: 'randallreilly_all',
 });
