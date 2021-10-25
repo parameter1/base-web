308b37dd7648bbe4eee3f54166a404b524ebd36f
Jacob Bare <jacob@parameter1.com>
2021-10-24 20:29:37 -0500

    Add docblocks

--- packages/immutable/src/wrap.js
+++ packages/immutable/src/wrap.js
@@ -4,16 +4,31 @@ const { log } = console;
 
 export default (obj) => {
   const map = fromJS(obj);
+  /**
+   *
+   * @param {string} path
+   * @returns {*|Map|List}
+   */
   const get = (path) => map.getIn(path.split('.'));
   return {
     log: log(obj),
     map,
     get,
+
+    /**
+     * @param {string} path
+     * @returns {List}
+     */
     getAsList: (path) => {
       const v = get(path);
       if (v instanceof List) return v;
       return List();
     },
+
+    /**
+     * @param {string} path
+     * @returns {Map}
+     */
     getAsMap: (path) => {
       const v = get(path);
       if (v instanceof Map) return v;
