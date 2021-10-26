78c6eaca6aed2b304061d5b0d1ec79781b8260e9
Jacob Bare <jacob@parameter1.com>
2021-10-25 23:38:17 -0500

    Create graphql utility package

---
+++ packages/graphql/src/index.js
@@ -0,0 +1,3 @@
+export { gql } from 'graphql-tag';
+export { default as extractFragmentData } from './extract-fragment-data.js';
+export { default as extractFragmentName } from './extract-fragment-name.js';
