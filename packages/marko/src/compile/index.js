fbdd7e1c9de6a8e31027345371b15e005cd18a9e
Jacob Bare <jacob@parameter1.com>
2021-11-18 08:28:10 -0600

    Add compile all function

--- packages/marko/src/compile/index.js
+++ packages/marko/src/compile/index.js
@@ -1,9 +1,11 @@
+const all = require('./all');
 const many = require('./many');
 const manySync = require('./many-sync');
 const one = require('./one');
 const oneSync = require('./one-sync');
 
 module.exports = {
+  all,
   one,
   oneSync,
   many,
