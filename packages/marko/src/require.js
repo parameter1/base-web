a164d1bca84c66b088dbdda7dd16a9418c168d25
Jacob Bare <jacob@parameter1.com>
2021-11-18 08:26:22 -0600

    Create node require

---
+++ packages/marko/src/require.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+require('marko');
+const { readFileSync } = require('fs');
+const compileSync = require('./compile/one-sync');
+
+const encoding = 'utf8';
+
+require.extensions['.marko'] = (module, filename) => {
+  const target = `${filename}.js`;
+  try {
+    const content = readFileSync(target, { encoding });
+    module._compile(content, target);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      // attempt to build from scratch using sync compile
+      // preferably all templates should be built ahead of time so this can be skipped
+      compileSync(filename);
+      module._compile(readFileSync(target, { encoding }), target);
+    }
+    throw e;
+  }
+};
