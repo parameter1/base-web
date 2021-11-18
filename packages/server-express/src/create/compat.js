0e1543195aa604ab82f133832dca59342ef0d0c9
Jacob Bare <jacob@parameter1.com>
2021-11-18 13:58:53 -0600

    Add compat todo logs

--- packages/server-express/src/create/compat.js
+++ packages/server-express/src/create/compat.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-param-reassign */
+
+const { log } = console;
+
 /**
  * Provides compat with @parameter1/base-cms-marko-web
  * @deprecated These globals should _not_ be used if possible.
@@ -6,6 +9,8 @@
 module.exports = ({ server, conf }) => {
   if (!conf.get('compat.enabled')) return;
   server.locals.tenantKey = conf.get('tenant.key');
+  log('@todo compat app.locals.config');
+  log('@todo compat app.locals.site');
   server.use((req, res, next) => {
     res.locals.requestOrigin = req.$requestOrigin;
     next();
