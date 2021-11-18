b0582cabb77c8126c386d0a3d75747e04f37d8df
Jacob Bare <jacob@parameter1.com>
2021-11-18 08:38:03 -0600

    Create delete all compiled files util

--- packages/marko/src/utils/index.js
+++ packages/marko/src/utils/index.js
@@ -1,4 +1,9 @@
+const deleteCompiledFiles = require('./delete-compiled-files');
 const findFiles = require('./find-files');
 const stat = require('./stat');
 
-module.exports = { findFiles, stat };
+module.exports = {
+  deleteCompiledFiles,
+  findFiles,
+  stat,
+};
