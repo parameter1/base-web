647da3ceb59552f9b7281b25cfe2c30ad1818d76
Jacob Bare <jacob@parameter1.com>
2021-11-18 07:45:50 -0600

    Add `getLastObject` function

--- packages/object-path/src/index.js
+++ packages/object-path/src/index.js
@@ -1,5 +1,6 @@
 const getAsArray = require('./get-as-array');
 const getAsObject = require('./get-as-object');
+const getLastObject = require('./get-last-object');
 const get = require('./get');
 const set = require('./set');
 const wrap = require('./wrap');
@@ -7,6 +8,7 @@ const wrap = require('./wrap');
 module.exports = {
   getAsArray,
   getAsObject,
+  getLastObject,
   get,
   set,
   wrap,
