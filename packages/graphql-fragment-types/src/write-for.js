f9e021a9f7138da9d543ec0ba008a5041c18882d
Jacob Bare <jacob@parameter1.com>
2021-11-18 09:28:18 -0600

    Fix json extension check

--- packages/graphql-fragment-types/src/write-for.js
+++ packages/graphql-fragment-types/src/write-for.js
@@ -6,7 +6,7 @@ module.exports = async ({ uri, headers, file } = {}) => {
   if (!file) throw new Error('A file location is required.');
   if (!path.isAbsolute(file)) throw new Error('The file location must be absolute.');
   const ext = path.extname(file);
-  if (ext !== 'json') throw new Error('The file must be JSON.');
+  if (ext !== '.json') throw new Error('The file must be JSON.');
   const types = await getFor({ uri, headers });
   await writeFile(file, JSON.stringify(types), { encoding: 'utf8' });
 };
