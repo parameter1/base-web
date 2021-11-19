95ddb4acd8b30a8c0f780ccfbc47d37fc8d7c15c
Jacob Bare <jacob@parameter1.com>
2021-11-19 15:25:08 -0600

    Serve public files

--- packages/server/express/src/create/static-files.js
+++ packages/server/express/src/create/static-files.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 
 module.exports = ({ server, conf }) => {
-  const dir = path.resolve(conf.get('cwd'), 'dist');
+  const cwd = conf.get('cwd');
+  const dir = path.resolve(cwd, 'dist');
   server.use('/dist/css', express.static(`${dir}/css`, { maxAge: '2y', immutable: true }));
   server.use('/dist/js', express.static(`${dir}/js`, { maxAge: '2y', immutable: true }));
+
+  server.use(express.static(path.join(cwd, 'server/public')));
 };
