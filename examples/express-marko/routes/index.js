973b6e9cbeb7f7f942b8cbe078a8794f7542639d
Jacob Bare <jacob@parameter1.com>
2021-11-19 13:46:46 -0600

    Render example template

--- examples/express-marko/routes/index.js
+++ examples/express-marko/routes/index.js
@@ -1,3 +1,5 @@
+const index = require('../templates/index.marko');
+
 module.exports = (server) => {
-  server.get('/', (_, res) => res.json({ hello: 'world' }));
+  server.get('/', (_, res) => res.marko(index));
 };
