1853ff1eacc7da50ef9de30d08686d90b9bb0afb
Jacob Bare <jacob@parameter1.com>
2021-11-18 22:06:38 -0600

    Distinguish between pre-built and on-the-fly templates

--- packages/marko/src/require.js
+++ packages/marko/src/require.js
@@ -1,22 +1,33 @@
 /* eslint-disable no-underscore-dangle */
 require('marko');
 const { readFileSync } = require('fs');
+const { emitWarning } = require('process');
 const compileSync = require('./compile/one-sync');
 
 const encoding = 'utf8';
 
+function emitCompileWarning() {
+  if (!emitCompileWarning.warned) {
+    emitCompileWarning.warned = true;
+    emitWarning('Template(s) compiled on-the-fly. Preferably, all templates should be compiled before running the server.');
+  }
+}
+
 require.extensions['.marko'] = (module, filename) => {
   const target = `${filename}.js`;
-  try {
-    const content = readFileSync(target, { encoding });
-    return module._compile(content, target);
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      // attempt to build from scratch using sync compile
-      // preferably all templates should be built ahead of time so this can be skipped
-      compileSync(filename);
-      return module._compile(readFileSync(target, { encoding }), target);
+  if (process.env.MARKO_PREBUILT_TEMPLATES) {
+    try {
+      const content = readFileSync(target, { encoding });
+      module._compile(content, target);
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        throw new Error(`Marko is running in prebuilt template mode, but no compiled template was found for ${filename}`);
+      }
+      throw e;
     }
-    throw e;
+  } else {
+    emitCompileWarning();
+    compileSync(filename);
+    module._compile(readFileSync(target, { encoding }), target);
   }
 };
