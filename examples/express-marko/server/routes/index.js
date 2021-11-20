ceeb6ef5eb58a4a8cd5fb6857404e0c008fbbd63
Jacob Bare <jacob@parameter1.com>
2021-11-20 13:50:25 -0600

    Add test ping route

--- examples/express-marko/server/routes/index.js
+++ examples/express-marko/server/routes/index.js
@@ -1,5 +1,19 @@
+const { asyncRoute } = require('@parameter1/base-web-marko-server-express/utils');
+const { gql } = require('@parameter1/base-web-graphql-lib/tag');
+const { jsonErrorHandler } = require('@parameter1/base-web-marko-server-express/middleware');
 const index = require('../templates/index.marko');
+
+const PING = gql`
+  query {
+    ping
+  }
+`;
 
 module.exports = (server) => {
   server.get('/', (_, res) => res.marko(index));
+
+  server.get('/graphql-ping', asyncRoute(async (req, res) => {
+    const { data } = await req.apollo.query({ query: PING });
+    res.json(data);
+  }), jsonErrorHandler());
 };
