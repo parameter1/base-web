4384d604658def233063c9eb68783d31b966616d
Jacob Bare <jacob@parameter1.com>
2021-11-20 09:18:16 -0600

    Send ready event after logging

--- packages/server/common/src/config/emit-ready.js
+++ packages/server/common/src/config/emit-ready.js
@@ -1,11 +1,11 @@
 const { log: l } = console;
 
 module.exports = ({ conf, log = l } = {}) => {
-  if (process.send) process.send({ event: 'ready', conf });
   log(`Env: ${conf.get('env') || '(not specified)'}`);
   log(`App: ${conf.get('app.name')} v${conf.get('app.version')}`);
   log(`Tenant: ${conf.get('tenant.key')}`);
   log(`Site ID: ${conf.get('site.id')}`);
   log(`BaseBrowse GraphQL URI: ${conf.get('baseBrowseGraphQLClient.uri')}`);
   log(`BaseCMS GraphQL URI: ${conf.get('baseCMSGraphQLClient.uri')}`);
+  if (process.send) process.send({ event: 'ready', conf });
 };
