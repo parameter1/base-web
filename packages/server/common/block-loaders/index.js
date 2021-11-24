b3410e9d00e5f0af77d08e2f8f0e3383eb387ecc
Jacob Bare <jacob@parameter1.com>
2021-11-24 09:44:01 -0600

    Move files to root of package

---
+++ packages/server/common/block-loaders/index.js
@@ -0,0 +1,7 @@
+const contentFromIdBlockLoader = require('./content-from-id');
+const websiteSectionFromAliasBlockLoader = require('./website-section-from-alias');
+
+module.exports = {
+  contentFromIdBlockLoader,
+  websiteSectionFromAliasBlockLoader,
+};
