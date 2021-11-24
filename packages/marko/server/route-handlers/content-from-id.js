4025e9fffd639be728c40e6d64fd312e44802f06
Jacob Bare <jacob@parameter1.com>
2021-11-24 15:33:13 -0600

    Add `routeData` prop in favor of spreading data

--- packages/marko/server/route-handlers/content-from-id.js
+++ packages/marko/server/route-handlers/content-from-id.js
@@ -1,6 +1,12 @@
 const { isFunction: isFn } = require('@parameter1/base-web-utils');
 const { contentFromId } = require('@parameter1/base-web-server-express/route-handlers');
 
+const maybeDeprecate = (marko, data) => {
+  if (!marko.get('compat.enabled')) return data;
+  process.emitWarning('Directly accessing content props (id, type, name, etc) within templates is deprecated. Use input.routeData[prop] instead.', { code: 'compat:input.routeData[prop]', type: 'DeprecationWarning' });
+  return { ...data.routeData, ...data };
+};
+
 module.exports = ({
   template,
   render,
@@ -14,8 +20,9 @@ module.exports = ({
   formatResponse,
 } = {}) => contentFromId({
   render: isFn(render) ? render : async ({ res, node, content }) => {
+    const { marko } = res.app.locals;
     if (isFn(formatResponse)) await formatResponse({ res, content, pageNode: node });
-    res.marko(template, { ...content, pageNode: node });
+    res.marko(template, maybeDeprecate(marko, { routeData: content, pageNode: node }));
   },
   idResolver,
   nodeQueryFragment,
--- packages/marko/server/route-handlers/website-section-from-alias.js
+++ packages/marko/server/route-handlers/website-section-from-alias.js
@@ -1,6 +1,12 @@
 const { isFunction: isFn } = require('@parameter1/base-web-utils');
 const { websiteSectionFromAlias } = require('@parameter1/base-web-server-express/route-handlers');
 
+const maybeDeprecate = (marko, data) => {
+  if (!marko.get('compat.enabled')) return data;
+  process.emitWarning('Directly accessing section props (id, alias, name, etc) within templates is deprecated. Use input.routeData[prop] instead.', { code: 'compat:input.routeData[prop]', type: 'DeprecationWarning' });
+  return { ...data.routeData, ...data };
+};
+
 module.exports = ({
   template,
   render,
@@ -17,13 +23,18 @@ module.exports = ({
     node: pageNode,
     section,
   }) => {
+    const { marko } = req.app.locals;
     const context = isFn(contextFn) ? await contextFn({
       req,
       res,
       section,
       pageNode,
     }) : {};
-    res.marko(template, { ...section, pageNode, context });
+    res.marko(template, maybeDeprecate(marko, {
+      routeData: section,
+      pageNode,
+      context,
+    }));
   },
 
   aliasResolver,
