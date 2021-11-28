6982389c1d3e0237d14213f9f1aeaec0a82cab9d
Jacob Bare <jacob@parameter1.com>
2021-11-27 21:19:26 -0600

    Create dynamic page from alias query

---
+++ packages/queries/dynamic-page-from-alias.js
@@ -0,0 +1,37 @@
+const gql = require('@parameter1/base-web-graphql-lib/tag');
+const { extractData } = require('@parameter1/base-web-graphql-lib/fragment');
+
+const defaultFragment = gql`
+  fragment DyanmicPageFromAliasBlockLoaderFragment on ContentPage {
+    id
+    alias
+  }
+`;
+
+const buildGraphQLOperation = ({ queryFragment } = {}) => {
+  const { spreadFragmentName, processedFragment } = extractData(queryFragment);
+  return gql`
+    query DynamicPageFromAliasBlockLoader($input: ContentPageQueryInput!) {
+      page: contentPage(input: $input) {
+        ...DyanmicPageBlockLoaderFragment
+        ${spreadFragmentName}
+      }
+    }
+    ${defaultFragment}
+    ${processedFragment}
+  `;
+};
+
+const executeQuery = async (baseCMSGraphQLClient, {
+  alias,
+  queryFragment,
+} = {}) => {
+  const input = { alias };
+  const { data } = await baseCMSGraphQLClient.query({
+    query: buildGraphQLOperation({ queryFragment }),
+    variables: { input },
+  });
+  return { node: data.page || null };
+};
+
+module.exports = { defaultFragment, buildGraphQLOperation, executeQuery };
