5fe88f17519ef48ad3cd85893b3eb3a35a2bbc20
Jacob Bare <jacob@parameter1.com>
2021-11-29 11:07:34 -0600

    Move marko global build to express lib

---
+++ packages/marko/lib/express/build-global.js
@@ -0,0 +1,54 @@
+const allowedGlobals = new Set([
+  'conf',
+  'marko',
+  'baseBrowseGraphQLClient',
+  'baseCMSGraphQLClient',
+  'request',
+  'contentPreviewModeEnabled',
+
+  // internals
+  'runtimeId',
+  'template',
+  '$wa',
+  '___components',
+  'componentIdPrefix',
+  'widgetIdPrefix',
+  'noBrowserRerender',
+  'oldHydrateNoCreate',
+  '__awaitReordererInvoked',
+  '___clientReorderContext',
+  '___isLastFlush',
+  '___didSerializeComponents',
+  '___typesLookup',
+  'serializedGlobals',
+  'cspNonce',
+]);
+
+/**
+ * Generates the marko `$global` template property.
+ *
+ * @param {object} res The Express response object.
+ */
+module.exports = (res, data) => {
+  const { req, app } = res;
+  const $global = {
+    app,
+    req,
+    res,
+    ...app.locals,
+    ...res.locals,
+  };
+
+  const hasWarned = new Set();
+  return (process.env.COMPAT_ENABLED) ? new Proxy({
+    ...$global, ...(data && data.$global),
+  }, {
+    get: (target, prop) => {
+      if (!allowedGlobals.has(prop) && typeof prop !== 'symbol' && !hasWarned.has(prop)) {
+        hasWarned.add(prop);
+        process.emitWarning(`Accessing out.global.${prop} within templates is deprecated. Remove or use alternative.`, { code: `compat:out.global.${prop}`, type: 'DeprecationWarning' });
+      }
+      return target[prop];
+    },
+  }) : { ...$global, ...(data && data.$global) };
+};
--- packages/marko/server/express/utils/index.js
+++ packages/marko/server/express/utils/index.js
@@ -1,4 +1,4 @@
 const { asyncRoute } = require('@parameter1/base-web-server-express/utils');
-const buildMarkoGlobal = require('./build-marko-global');
+const buildMarkoGlobal = require('@parameter1/base-web-marko-lib/express/build-global');
 
 module.exports = { asyncRoute, buildMarkoGlobal };
