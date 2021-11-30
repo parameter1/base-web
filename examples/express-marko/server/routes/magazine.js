cfef199ba521f2b4afeff235b6be15ac84dd366a
Jacob Bare <jacob@parameter1.com>
2021-11-30 11:17:21 -0600

    Add magazine publication example

--- examples/express-marko/server/routes/magazine.js
+++ examples/express-marko/server/routes/magazine.js
@@ -1,9 +1,17 @@
-const { withMagazineIssue } = require('@parameter1/base-web-marko-server-express/middleware');
+const { magazineIssueFromId, magazinePublicationFromId } = require('@parameter1/base-web-marko-server-express/route-handlers');
+const publicationFragment = require('../graphql/fragments/magazine-publication-page');
 const issueFragment = require('../graphql/fragments/magazine-issue-page');
+
 const issue = require('../templates/magazine-issue.marko');
+const publication = require('../templates/magazine-publication.marko');
 
 module.exports = (server) => {
-  server.get('/magazine/:id(\\d+)', withMagazineIssue({
+  server.get('/magazine/:id([a-fA-F0-9]{24})', magazinePublicationFromId({
+    template: publication,
+    queryFragment: publicationFragment,
+  }));
+
+  server.get('/magazine/:id(\\d+)', magazineIssueFromId({
     template: issue,
     queryFragment: issueFragment,
   }));
