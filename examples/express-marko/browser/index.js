9213532f1cb6b06bdb8ba420439eaf01f03f3e2a
Jacob Bare <jacob@parameter1.com>
2021-11-30 14:59:20 -0600

    Include social share plugin with directly imported scss

--- examples/express-marko/browser/index.js
+++ examples/express-marko/browser/index.js
@@ -1,3 +1,7 @@
 import Browser from '@parameter1/base-web-marko-components-core/browser';
 
+import SocialSharing from '@parameter1/base-web-marko-plugin-social-sharing/browser';
+
+SocialSharing(Browser, { importSCSS: true });
+
 export default Browser;
